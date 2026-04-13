3b045b70c0155ddbda629372b3a368edf7fd53a7
pwegrzynowicz <patrycja@yonlabs.com>
2026-04-13 17:40:28 +0200

    chore: remove tests as they are not up-to-date

--- tests/test_cluster_generator.py
+++
@@ -1,9 +0,0 @@
-from abc_benchmark.generation.cluster_generator import make_generator
-
-
-def test_generator_produces_scene():
-    gen = make_generator("easy")
-    scene = gen.generate(seed=123, difficulty_name="easy")
-    assert scene.gold_label >= 0
-    assert len(scene.items) > 0
-    assert "Respond with a number only" in scene.prompt
