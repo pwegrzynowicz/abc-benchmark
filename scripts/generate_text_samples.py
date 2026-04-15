e7ddea3e0b191ce67f53b271e8c10feb2dacfaac
pwegrzynowicz <patrycja@yonlabs.com>
2026-04-15 14:45:49 +0200

    chore: clean up

--- scripts/generate_text_samples.py
+++
@@ -1,6 +0,0 @@
-from abc_benchmark.generation.feature_text_generator import make_generator
-
-gen = make_generator("medium")
-scene = gen.generate(seed=42, difficulty_name="medium")
-print(scene.prompt)
-print("gold:", scene.gold_label)
